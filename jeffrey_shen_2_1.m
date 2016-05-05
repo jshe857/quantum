48fef8727a55529896aeaa29a8148259759ba756
jshe857 <jeffrey_shen88@hotmail.com>
2016-05-05 22:52:45 +1000

    added q 2.5

--- jeffrey_shen_2_1.m
+++ jeffrey_shen_2_1.m
@@ -26,8 +26,7 @@ phi_2R = sqrt(2/a) * sin(2*pi * (x_R - l) / a);
 %% Plot the results.    
 figure(1)
 %subplot(2,2,1)
-plot(x_L,abs(phi_1L).^2,x_L,abs(phi_2L).^2,x_R,abs(phi_1R).^2,x_R,abs(phi_2R).^2);
-figure(2) 
+plot(x_L,abs(phi_1L).^2,x_L,abs(phi_2L).^2,x_R,abs(phi_1R).^2,x_R,abs(phi_2R).^2); 
 title('Stationary states of 2 infinite potential wells');
 xlabel('x (nm)');
 ylabel('P(x)');
